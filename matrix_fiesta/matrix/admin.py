08bc206110ad74a085f49f2eaec4cb79fb099bce
giliam <matthieu.denoux@gmail.com>
2019-07-11 16:19:32 +0200

    Updates admin to have autofilling slug fields.

--- matrix_fiesta/matrix/admin.py
+++ matrix_fiesta/matrix/admin.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 
 from matrix import models
 
+class SlugNomAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("nom",)}
+
+
 
 admin.site.register(models.EchelleValeurs)
 admin.site.register(models.EvaluationEleve)
@@ -10,5 +14,5 @@ admin.site.register(models.Utilisateur)
 admin.site.register(models.Annee)
 admin.site.register(models.Semestre)
 admin.site.register(models.UE)
-admin.site.register(models.ECUE)
-admin.site.register(models.AcquisApprentissage)
+admin.site.register(models.ECUE, SlugNomAdmin)
+admin.site.register(models.AcquisApprentissage, SlugNomAdmin)
