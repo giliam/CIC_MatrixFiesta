c5e9864a0e6c9d028805228dbaa302b5539e41b3
giliam <matthieu.denoux@gmail.com>
2019-08-22 08:53:20 +0200

    Reverts to previous system with lazy translations.

--- matrix_fiesta/matrix/models.py
+++ matrix_fiesta/matrix/models.py
@@ -3,7 +3,7 @@ import datetime
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils.text import slugify
-from django.utils.translation import gettext as _
+from django.utils.translation import ugettext_lazy as _
 
 from common.auths import GroupsNames
 
